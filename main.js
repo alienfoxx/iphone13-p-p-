57232786b64e805e40faacbfd35cdd0df8973fa5
alienfoxx <alienfoxx@gmail.com>
2023-05-09 20:01:13 +0100

    iphone-13 product page

---
+++ main.js
@@ -0,0 +1,9 @@
+let img=document.querySelector(".main");
+let background =document.querySelector(".container");
+function phones(phone){
+    img.src=phone;
+}
+function colors(color){
+    background.style.background= color;
+
+}
